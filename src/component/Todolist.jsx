6dcc8b773bbbdbe4dbd36fac958e26737f8ed059
likeprograming1 <hsj2216@naver.com>
2022-09-30 18:39:38 +0900

    캘린더 구현중...

--- src/component/Todolist.jsx
+++ src/component/Todolist.jsx
@@ -1,129 +1,178 @@
-import { useCallback } from "react";
-import { useState } from "react"
+import React, { useCallback, useState } from "react";
+import classNames from "classnames/bind";
+import style from "../todolist.css";
+
+const cx = classNames.bind(style);
 
 export const Todolist = () => {
   const today = {
-    year: new Date().getFullYear(), // 이번연도
-    month: new Date().getMonth()+1, // 이번달
-    day: new Date().getDay(),// 이번달 요일
-    date: new Date().getDate() // 오늘 날짜
-  }
-  
-  const week = ['일', '월', '화', '수', '목', '금', '토']; // 일주일
-  const [selectedYear, setselectedYear] = useState(today.year); // 현재 선택된 연도
-  const [selectedMonth, setselectedMonth] = useState(today.month) // 현재 선택된 달
+    year: new Date().getFullYear(), //오늘 연도
+    month: new Date().getMonth() + 1, //오늘 월
+    date: new Date().getDate(), //오늘 날짜
+    day: new Date().getDay(), //오늘 요일
+  };
+  const week = ["일", "월", "화", "수", "목", "금", "토"]; //일주일
+  const [selectedYear, setSelectedYear] = useState(today.year); //현재 선택된 연도
+  const [selectedMonth, setSelectedMonth] = useState(today.month); //현재 선택된 달
+  const dateTotalCount = new Date(selectedYear, selectedMonth, 0).getDate(); //선택된 연도, 달의 마지막 날짜
 
-  const dateTotalCount = new Date(selectedYear, selectedMonth, 0).getDate
-  // 선택된 연도, 달의 마지막 날짜
-
-  const prevMonth = useCallback(() => { // 이전 달 보기 버튼
-    if(selectedMonth === 1){
-      setselectedMonth(12);
-      setselectedYear(selectedYear - 1);
-    }else {
-      setselectedMonth(selectedMonth + 1);
+  const prevMonth = useCallback(() => {
+    //이전 달 보기 보튼
+    if (selectedMonth === 1) {
+      setSelectedMonth(12);
+      setSelectedYear(selectedYear - 1);
+    } else {
+      setSelectedMonth(selectedMonth - 1);
     }
   }, [selectedMonth]);
 
-  const nextMonth = useCallback(()=>{ // 다음 달 보기 버튼
-    if(selectedMonth === 12){
-      setselectedMonth(1);
-      setselectedYear(selectedYear + 1);
-    }else {
-      setselectedMonth(selectedMonth + 1);
+  const nextMonth = useCallback(() => {
+    //다음 달 보기 버튼
+    if (selectedMonth === 12) {
+      setSelectedMonth(1);
+      setSelectedYear(selectedYear + 1);
+    } else {
+      setSelectedMonth(selectedMonth + 1);
     }
   }, [selectedMonth]);
 
-  const monthControl = useCallback(()=>{ // 달 선택박스에서 고르기
+  const monthControl = useCallback(() => {
+    //달 선택박스에서 고르기
     let monthArr = [];
-    for(let i = 1; i <= 12; i++){
+    for (let i = 0; i < 12; i++) {
       monthArr.push(
-        <option key={i} value={i}>
-          {i}월
+        <option key={i + 1} value={i + 1}>
+          {i + 1}월
         </option>
-      )
+      );
     }
-  }, [selectedMonth])
-  
-  const yearControl = useCallback(()=>{// 연도 선택박스에서 고르기
-    let yearArr = [];
-    const startYear = today.year - 10;
-    const endYear = today.year + 10;
+    return (
+      <select
+        onChange={changeSelectMonth}
+        value={selectedMonth}
+      >
+        {monthArr}
+      </select>
+    );
+  }, [selectedMonth]);
 
-    for(let i = startYear; i < endYear; i++){
+  const yearControl = useCallback(() => {
+    //연도 선택박스에서 고르기
+    let yearArr = [];
+    const startYear = today.year - 10; //현재 년도부터 10년전 까지만
+    const endYear = today.year + 10; //현재 년도부터 10년후 까지만
+    for (let i = startYear; i < endYear + 1; i++) {
       yearArr.push(
         <option key={i} value={i}>
           {i}년
         </option>
-      )
+      );
     }
-    
     return (
       <select
-        className="yearselect"
+        // className="yearSelect"
         onChange={changeSelectYear}
         value={selectedYear}
       >
         {yearArr}
       </select>
-    )
-  },[selectedYear])
+    );
+  }, [selectedYear]);
 
   const changeSelectMonth = (e) => {
-    setselectedMonth(Number(e.target.value))
-  }
-
+    setSelectedMonth(Number(e.target.value));
+  };
   const changeSelectYear = (e) => {
-    selectedYear(Number(e.target.value))
-  }
+    setSelectedYear(Number(e.target.value));
+  };
 
-  const returnWeek = useCallback(()=> { //요일 변환 함수
+  const returnWeek = useCallback(() => {
+    //요일 반환 함수
     let weekArr = [];
-    week.forEach((v)=>{
+    week.forEach((v) => {
       weekArr.push(
         <div
-          key={v}//추가해야함
+          key={v}
+          className={cx(
+            { weekday: true },
+            { sunday: v === "일" },
+            { saturday: v === "토" }
+          )}
         >
           {v}
         </div>
-      )
-    })
+      );
+    });
     return weekArr;
-  }, [])
+  }, []);
 
-  const returnDay = useCallback(()=> { // 선택된 달의 날짜들 반환 함쑤
+  const returnDay = useCallback(() => {
+    //선택된 달의 날짜들 반환 함수
     let dayArr = [];
 
-    for(const nowDay of week){
-      const day = new Date(selectedYear, selectedMonth -1, 1).getDay();
-      if(week[day] === nowDay){
-        for(let i = 1; i <= dateTotalCount; i++){
+    for (const nowDay of week) {
+      const day = new Date(selectedYear, selectedMonth - 1, 1).getDay();
+      if (week[day] === nowDay) {
+        for (let i = 0; i < dateTotalCount; i++) {
           dayArr.push(
             <div
-              key={i}
+              key={i + 1}
+              className={cx(
+                {
+                  //오늘 날짜일 때 표시할 스타일 클라스네임
+                  today:
+                    today.year === selectedYear &&
+                    today.month === selectedMonth &&
+                    today.date === i + 1,
+                },
+                { weekday: true }, //전체 날짜 스타일
+                {
+                  //전체 일요일 스타일
+                  sunday:
+                    new Date(
+                      selectedYear,
+                      selectedMonth - 1,
+                      i + 1
+                    ).getDay() === 0,
+                },
+                {
+                  //전체 토요일 스타일
+                  saturday:
+                    new Date(
+                      selectedYear,
+                      selectedMonth - 1,
+                      i + 1
+                    ).getDay() === 6,
+                }
+              )}
             >
-              {i}
+              {i + 1}
             </div>
           );
         }
-      }else {
-        dayArr.push(<div className="weekday"></div>)
+      } else {
+        dayArr.push(<div className="weekday"></div>);
       }
     }
+
     return dayArr;
   }, [selectedYear, selectedMonth, dateTotalCount]);
-  
+
   return (
     <div className="container">
       <div className="title">
-        <h3>{yearControl()}년 {monthControl()}월</h3>
-        <button onClick={prevMonth}>◀︎</button>
-        <button onClick={nextMonth}>▶︎</button>
+      <div className="pagination">
+        <button className="btn" onClick={prevMonth}>◀︎</button>
+      </div>
+        <h3>
+          {yearControl()}년 {monthControl()}월
+        </h3>
+        <div className="pagination">
+          <button className="btn" onClick={nextMonth}>▶︎</button>
+        </div>
       </div>
       <div className="week">{returnWeek()}</div>
       <div className="date">{returnDay()}</div>
     </div>
   );
 };
-
-export default Todolist;
